9cb06d99ed6a97572f84bdcf0e02a3cb37e22d19
shravani1708 <113713917+shravani1708@users.noreply.github.com>
2023-03-21 15:53:23 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install requests bs4
+
+
+# In[39]:
+
+
+import requests
+from bs4 import BeautifulSoup 
+import json
+stocks={'AMZN','DWAC','META','AAPL'}
+stockinfo=[]
+def getdata(company):
+    headers= {'User-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    url=f'https://finance.yahoo.com/quote/{company}'  
+    res =requests.get(url,headers=headers) 
+    soup=BeautifulSoup(res.text,'html.parser')
+    stock={
+        'company':company , 
+        'price':soup.find('div',{'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
+        'change':soup.find('div',{'class':'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text,
+    }
+    return stock
+for item in stocks:
+    stockinfo.append(getdata(item))
+n=len(stockinfo)
+for i in range(n):
+    print(item,'=',stockinfo[i])
+print('Success')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
